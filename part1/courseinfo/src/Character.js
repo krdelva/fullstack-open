beace509194213eea7608adfeabca55baf911b1b
krdelva <kevin.r.delva@gmail.com>
2022-11-08 13:00:43 -0400

    Fix poits/attributes functionality Need to find a way to refactor code, since it looks messy

--- part1/courseinfo/src/Character.js
+++ part1/courseinfo/src/Character.js
@@ -13,7 +13,23 @@ const Character = ({player, setPlayer}) => {
     const handleClick = () => {
         // "message" stores input field value
         setUpdated(message);
-        setPlayer({...player, name: message});
+        setPlayer({...player, name: message, points: player.points + 1});
+    }
+
+    const subtract = (comp ,att) => {
+        if (comp > 1) {
+            setPlayer(att)
+        } else {
+            console.log("Can't subtract")
+        }
+    }
+
+    const add = (att) => {
+        if (player.points > 0) {
+            setPlayer(att);
+        } else {
+            console.log("No points!");
+        }
     }
 
     
@@ -21,10 +37,26 @@ const Character = ({player, setPlayer}) => {
         <>
             <h2>{player.name}</h2>
             <h3>Points: {player.points}</h3>
-            <div style={{display: "flex"}}><Button onClick={() => setPlayer({...player, hp: player.hp - 1})} text="-" /><p>HP: {player.hp}</p><Button onClick={() => setPlayer({...player, hp: player.hp + 1})}  text="+" /></div>
-            <div style={{display: "flex"}}><Button onClick={() => setPlayer({...player, mp: player.mp - 1})} text="-" /><p>MP: {player.mp}</p><Button onClick={() => setPlayer({...player, mp: player.mp + 1})} text="+" /></div>
-            <div style={{display: "flex"}}><Button onClick={() => setPlayer({...player, atk: player.atk - 1})} text="-" /><p>ATK: {player.atk}</p><Button onClick={() => setPlayer({...player, atk: player.atk + 1})} text="+" /></div>
-            <div style={{display: "flex"}}><Button onClick={() => setPlayer({...player, def: player.def - 1})} text="-" /><p>DEF: {player.def}</p><Button onClick={() => setPlayer({...player, def: player.def + 1})} text="+" /></div>
+            <div style={{display: "flex"}}>
+                <Button onClick={() => subtract(player.hp, {...player, hp: player.hp - 1, points: player.points + 1})} text="-" />
+                <p>HP: {player.hp}</p>
+                <Button onClick={() => add({...player, hp: player.hp + 1, points: player.points - 1})}  text="+" />
+            </div>
+            <div style={{display: "flex"}}>
+                <Button onClick={() => subtract(player.mp, {...player, mp: player.mp - 1, points: player.points + 1})} text="-" />
+                <p>MP: {player.mp}</p>
+                <Button onClick={() => add({...player, mp: player.mp + 1, points: player.points - 1})} text="+" />
+            </div>
+            <div style={{display: "flex"}}>
+                <Button onClick={() => subtract(player.atk, {...player, atk: player.atk - 1, points: player.points + 1})} text="-" />
+                <p>ATK: {player.atk}</p>
+                <Button onClick={() => add({...player, atk: player.atk + 1, points: player.points - 1})} text="+" />
+            </div>
+            <div style={{display: "flex"}}>
+                <Button onClick={() => subtract(player.def, {...player, def: player.def - 1, points: player.points + 1})} text="-" />
+                <p>DEF: {player.def}</p>
+                <Button onClick={() => add({...player, def: player.def + 1, points: player.points - 1})} text="+" />
+            </div>
 
             <label>Hello:</label>
             <input 
